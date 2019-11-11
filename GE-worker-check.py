e41e8fcc5e19b3385f53bc3938a057b1d14f04de
Jones-Thomas <42827211+Jones-Thomas@users.noreply.github.com>
2019-11-11 17:06:34 -0500

    Update GE-worker-check.py

--- GE-worker-check.py
+++ GE-worker-check.py
@@ -6,8 +6,8 @@ from psutil._common import bytes2human
 import datetime
 import time
 
-ip = ["10.2.1.154","10.2.1.155","10.2.1.156"]
-username = "ge-dev\jthomas-admin"
+ip = ["10.2.1.1*4","10.2.1.*55","10.2.1.1*6"]
+username = "Domain\user"
 password = "7qAaNHewf8AP8kK#$@"
 mem = psutil.virtual_memory()
 mem_total = bytes2human(mem.total)
@@ -16,6 +16,7 @@ mem_used = bytes2human(mem.used)
 mem_free = bytes2human(mem.free)
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+final_out=""
 
 for h in ip:
   try:
@@ -24,8 +25,9 @@ for h in ip:
     #print ("Connection established")
     for i in connection.Win32_service():
        if i.Caption == 'globaledit Worker':
-        Final_out = print (st ,"-", h, i.SystemName, i.Caption,":", i.State,"->", i.Status,":", "Mem_Total: " + mem_total , "Mem_Avail: " + mem_avail, "Mem_Used: " + mem_used, "Mem_Free: " + mem_free)
-        f= open('Stg-worker-check.txt', 'a+')
-        print (final_out , file=f)
+          print (st ,"-", h,"-", i.SystemName,"-", i.Caption,":", i.State,"~", i.Status,"-->", " Mem_Total: " + mem_total , " Mem_Avail: " + mem_avail, " Mem_Used: " + mem_used, "Mem_Free: " + mem_free)
+          final_out = (st ,"-", h,"-", i.SystemName,"-", i.Caption,":", i.State,"~", i.Status,"-->", " Mem_Total: " + mem_total , " Mem_Avail: " + mem_avail, " Mem_Used: " + mem_used, "Mem_Free: " + mem_free)
+    with open('Stg-worker-check.txt', 'a+') as f:
+         print (final_out , file=f)
   except wmi.x_wmi:
     print ("Your Username and Password of "+getfqdn(ip)+" are wrong.")
